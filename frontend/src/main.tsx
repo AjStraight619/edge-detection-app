ec0d7bed8d12f046636c45d552d7b538056102ef
Alex Straight <AlexStraight619@gmail.com>
2025-03-19 14:48:55 -0700

    chore: Rename to EdgeDetectionApp

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+import EdgeDetectionApp from "./App.tsx";
 import { ThemeProvider } from "./providers/theme-provider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="theme">
-      <App />
+      <EdgeDetectionApp />
     </ThemeProvider>
   </StrictMode>
 );
